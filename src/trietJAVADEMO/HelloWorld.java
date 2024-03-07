4e6bf337beda03f5e46e9b10496035518792d5cf
TranDoanTriet <trandoantriet@gmail.com>
2024-03-07 09:24:54 +0700

    cac ham toan hoc

--- src/trietJAVADEMO/HelloWorld.java
+++ src/trietJAVADEMO/HelloWorld.java
@@ -1,7 +1,16 @@
 package trietJAVADEMO;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("so dau tien: ");
+		int s1 = scanner.nextInt();
+		System.out.println("so thu 2: ");
+		int s2 = scanner.nextInt();
+		System.out.print(Math.sqrt(s1));
+		scanner.close();
 	}
 }
